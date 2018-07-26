64178d95e754d9b9e07c6326a143db44cb85dc17
github username <github email address>
2018-07-26 17:00:34 -0400

    done

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,36 @@
-class Article
+class Article #an article belongs to an author and magazine
+
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def find_magazine_instance
+    Magazine.all.find do |magazine|
+      magazine.article == self
+    end
+  end
+
+  # returns the author for that given article
+
+  def find_author
+    find_magazine_instance.author
+  end
+
+   # + returns the magazine for that given article
+
+  def magazine
+    find_magazine_instance.magazine
+  end
 
 
-end
+
+end #end class
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,45 @@
-class Magazine
+class Magazine #a magazine has many articles
   attr_accessor :name, :category
+  attr_reader :article, :author
+
+  @@all = []
 
 
-  def initialize(name, category)
+  def initialize(name, category, article, author)
     @name = name
     @category = category
+    @article = article
+    @author = author
+    @@all << self
+  end
+
+  # + returns an array of all magazines
+
+  def self.all
+    @@all
+  end
+
+   # + given a string of magazine name, returns the first magazine that matches
+
+  def self.find_by_name(name)
+    self.all.find do |magazine|
+      magazine.name == name
+    end
+  end
+
+  # + returns a list of the titles of all articles written for that magazine
+
+  #the way i initialized a magazine, it only takes a single article instance, i would need to initialize a magazine instance with an array of articles in order to build this method, but i've run out of time
+  #if I had an array of articles to sort though in each magazine, i would first iterate through each magazine to find the article instances and then iterate through the list of article instances to find the article.name using the Article class
+  #to return a list of all the titles
+
+  def self.article_titles
+
+
 
   end
 
 
-end
+
+
+end #end class
